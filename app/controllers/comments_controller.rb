53fb9b9e269e9b82e85ed8e45c0ef3c99e23d3bc
Linxerz <pruittj2000@yahoo.com>
2020-04-13 15:07:27 -0500

    Showing comments now

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,17 @@
 class CommentsController < ApplicationController
-    def edit
-    end
-
-    def index
-    end
-
-    def update
-    end
-    
-    def new
-    end
 
     def create
-        byebug
+
+        @comment = Comment.create({
+            content: params[:content],
+            post_id: params[:post_id],
+            user_id: params[:user_id]
+        })
+        redirect_to("/posts/#{params[:post_id]}")
     end
+
+
+    # I don't think we'll need anymore routes for comments if we are making the comments in the posts 'show' page
 
 
 end
