4d184957eb1d5b1ed7381f80fe8d3adf5d5ea6d3
Linxerz <pruittj2000@yahoo.com>
2020-04-13 11:15:38 -0500

    Added href_paths in show for User, Projects, and Posts. Also shitty formatting stuff but logically can iterate through stuff

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,13 @@
 class CommentsController < ApplicationController
+    def edit
+    end
+
+    def update
+    end
     
+    def new
+    end
+
+    def create
+    end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,6 @@
 class ProjectsController < ApplicationController
-
+    def show
+        @project = Project.find(params[:id])
+        @user = User.find(@project.user_id)
+    end
 end
