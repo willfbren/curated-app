0d5befb3b9c4ae50a5a14c434f2ece83fbb82500
willfbren <willfbren@gmail.com>
2020-04-14 16:00:04 -0500

    added sign up and login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    before_action(:check_login)
+
+    def check_login
+        if session[:user_id] == nil
+            redirect_to '/login'
+        else
+            @current_user = User.find(session[:user_id])
+        end
+    end 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_215634) do
+ActiveRecord::Schema.define(version: 2020_04_14_181301) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -42,8 +42,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_215634) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
-    t.string "confirm_password"
+    t.string "password_digest"
   end
 
 end
