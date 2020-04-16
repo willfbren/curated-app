c81dade75ac80ba6e779e0f7b80b64f567ca63ac
willfbren <willfbren@gmail.com>
2020-04-15 21:10:39 -0500

    added update/delete functionality for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,18 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:new, :edit, :show, :create, :update, :destroy,:index]
+  resources :posts, only: [:new, :edit, :show, :create, :update, :destroy, :index]
   resources :comments, :categories, :projects, :users
 
   get '/', to: 'posts#index'
-  
-  # get '/login', to: 'users#view_login'
-
-  # post '/login', to: 'users#handle_login'
 
   get '/users/:id/projects', to: 'users#user_projects'
 
   get '/users/:id/projects/new', to: 'projects#new'
 
-  get '/users/:id/projects/:project_id/edit', to: 'projects#edit'
+  get '/projects/:id/edit', to: 'projects#edit'
 
-  patch '/users/:id/projects/:project_id', to: 'projects#update'
+  patch 'projects/:id', to: 'projects#update'
 
   get '/users/:id/posts', to: 'users#user_posts'
 
@@ -25,8 +21,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
 
   post '/login', to: 'sessions#create'
-
-  # get '/logout', to: 'sessions#logout'
 
   get '/logout', to: 'sessions#destroy'
 
